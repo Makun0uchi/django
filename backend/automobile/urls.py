9296122525a881b0b2557b3721cc2cf6577eb489
Makun0uchi <vladik.volkov.2016@bk.ru>
2023-07-16 07:04:13 +0300

    urls
    Растаскал урлы по приложениям.

---
+++ backend/automobile/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from automobile.views import *
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+
+router = routers.DefaultRouter()
+router.register(r'carlist', CarViewSet)
+router.register(r'sparelist', SpareViewSet)
+router.register(r'countrylist', CountryViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls))
+]
--- backend/car/urls.py
+++ backend/car/urls.py
@@ -29,7 +29,6 @@ router.register(r'countrylist', CountryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('', include('automobile.urls')),
+    path('', include('user.urls'))
 ]
