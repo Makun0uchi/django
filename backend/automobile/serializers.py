e6dabad48c6637177b164a69cc010fc2ba86ea13
Makun0uchi <vladik.volkov.2016@bk.ru>
2023-07-17 20:05:36 +0300

    Бэкэнд
    Изменил сеттинги и сериализаторы для корректной работы бэка.

--- backend/automobile/serializers.py
+++ backend/automobile/serializers.py
@@ -15,6 +15,8 @@ class CountrySerializer(serializers.ModelSerializer):
 
 
 class CarSerializer(serializers.ModelSerializer):
+    country_of_origin = serializers.SlugRelatedField(slug_field='country_name', queryset=Country.objects.all())
+    car_details = serializers.SlugRelatedField(slug_field='spares_name', many=True, queryset=Spare.objects.all())
     class Meta:
         model = Car
         fields = '__all__'
